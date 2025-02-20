971c6710740f0b34571f902261dfa7cadb4cf1fa
Julien Flusin <julienflusin3@gmail.com>
2025-02-20 11:07:29 +0100

    V1 test

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# Stage 1: Build dependencies
+FROM node:20-alpine AS build
+LABEL org.opencontainers.image.description "A simple Node.js web server that uses yt-dlp to extract subs from a youtube video."
+
+# Set the working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json to install dependencies
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application files
+COPY . .
+
+# Stage 2: runtime
+FROM node:20-alpine AS runtime
+
+# Set the working directory
+WORKDIR /app
+
+# Install only necessary Puppeteer dependencies
+RUN apk add --no-cache \
+    && rm -rf /var/cache/*
+
+# Install yt-dlp
+RUN curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp &&  chmod +x /usr/local/bin/yt-dlp
+
+# Copy node_modules and application code from the build stage
+COPY --from=build /app /app
+
+# Expose the port your app runs on
+EXPOSE 3000
+
+# Command to run the server
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Utility function to pause execution for a set amount of time
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const downloadTranscript = async (video_url, language) => {
+    // use system yt-dlp command to download the transcript
+    const command = `/usr/local/bin/yt-dlp --write-subs --write-auto-subs --skip-download --sub-langs ${language} ${video_url}`;
+    console.log("Running command:", command);
+
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error downloading transcript: ${error.message}`);
+                reject(error);
+            }
+
+            if (stderr) {
+                console.error(`Error downloading transcript: ${stderr}`);
+                reject(stderr);
+            }
+
+            console.log(`Transcript downloaded: ${stdout}`);
+            resolve(stdout);
+        });
+    });
+};
+
+
+// Screenshot route
+app.get('/download_transcript', async (req, res) => {
+    const $video_url = req.query.video_url;
+    const $language = req.query.language || 'en';
+
+    if (!$video_url || $video_url === '' || !$language || $language === '') {
+        return res.status(400).send('Video URL and language parameters are required');
+    }
+
+    try {
+        // run a system command to download the transcript
+        const transcript = await downloadTranscript($video_url, $language);
+
+        res.setHeader('Content-Type', 'text/plain');
+        res.send(transcript);
+    } catch (error) {
+        console.error("Error downloading transcript:", error);
+        res.status(500).send('Failed to download transcript');
+    }
+});
+
+// Start the Express server
+app.listen(PORT, async () => {
+    try {
+        await initBrowser();
+        console.log(`Server running on port ${PORT}`);
+    } catch (error) {
+        console.error("Failed to initialize server:", error);
+        process.exit(1);
+    }
+});
